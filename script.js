a484d516b9cc49609b25b7026f98d9de7cfc10e7
snnvy <114437076+snnvy@users.noreply.github.com>
2022-11-03 12:54:09 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "100%";
+    document.getElementById("header").style.marginTop = "190px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("header").style.marginTop = "0";
+  }
+  
